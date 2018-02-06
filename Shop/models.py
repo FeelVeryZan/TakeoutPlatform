5145426175786d8c75b9d052a7359c944be24003
nodgd <dcc1234@qq.com>
2018-02-06 23:17:58 +0800

    写好了数据库里面的模型

--- Shop/models.py
+++ Shop/models.py
@@ -1,3 +1,111 @@
+# coding: utf-8
 from django.db import models
 
-# Create your models here.
+from User.models import toStringWithoutError
+
+# 店铺的信息
+class Shops:
+    # 店铺的在数据库中的编号
+    id = models.IntegerField(primary_key=True)
+    # 店铺的名称
+    name = models.CharField(max_length=256, default='暂无名称')
+    # 店铺的简介
+    description = models.CharField(max_length=65536, default='暂无简介')
+    # 店铺的图标
+    logo = models.ImageField(null=True, default=None)
+    # 店铺的地址
+    address = models.TextField(max_length=256, default='暂无地址')
+    # 店铺的电话
+    phoneNumber = models.TextField(max_length=256, default='暂无电话')
+    # 店铺的老板数量
+    ownerCnt = models.IntegerField()
+    # 店铺老板（们）的用户编号，用 "编号|编号|...|编号" 的形式存储
+    ownerListStr = models.TextField(max_length=256)
+    # 店铺注册的时间
+    signUpTime = models.DateTimeField(auto_now_add=True)
+    # 店铺的商品数量
+    goodsCount = models.IntegerField(default=0)
+    # 店铺的总销售量
+    totalSales = models.IntegerField(default=0)
+    # 店铺的月销售量
+    monthlySales = models.IntegerField(default=0)
+    # 店铺的周销售量
+    weeklySales = models.IntegerField(default=0)
+    # 店铺的日销售量
+    dailySales = models.IntegerField(default=0)
+    # 获取老板编号列表，[编号,编号,...]
+    def getOwnerList(self):
+        ownerStrList = self.ownerListStr.split('|')
+        ownerList = []
+        for ownerStr in ownerStrList:
+            ownerList.append(int(ownerStr))
+        return ownerList
+    # 调试输出
+    def __str__(self):
+        return 'Shop.models.Shops(' + [
+            'id=' + toStringWithoutError(self.id),
+            'name=' + toStringWithoutError(self.name),
+            'description=' + toStringWithoutError(self.description),
+            'logo=' + toStringWithoutError(self.logo),
+            'address=' + toStringWithoutError(self.address),
+            'phoneNumber=' + toStringWithoutError(self.phoneNumber),
+            'ownerCnt=' + toStringWithoutError(self.ownerCnt),
+            'ownerListStr=' + toStringWithoutError(self.ownerListStr),
+            'signUpTime=' + toStringWithoutError(self.signUpTime),
+            'goodsCount=' + toStringWithoutError(self.goodsCount),
+            'totalSales=' + toStringWithoutError(self.totalSales),
+            'monthlySales=' + toStringWithoutError(self.monthlySales),
+            'weeklySales=' + toStringWithoutError(self.weeklySales),
+            'dailySales=' + toStringWithoutError(self.dailySales),
+        ].join(', ') + ')'
+
+
+# 商品的信息
+class Goods:
+    # 商品在数据库中的编号
+    id = models.IntegerField(primary_key=True)
+    # 商品的名称
+    name = models.CharField(max_length=256, default='暂无名称')
+    # 商品的简介
+    description = models.CharField(max_length=65536, default='暂无简介')
+    # 商品的样例图片
+    picture = models.ImageField(null=True, default=None)
+    # 商品所属的店铺编号
+    shopId = models.IntegerField()
+    # 商品添加的时间
+    addTime = models.DateTimeField(auto_now_add=True)
+    # 商品的评分次数
+    scoreCnt = models.FloatField(default=0)
+    # 商品的评分总和
+    scoreSum = models.IntegerField(default=0)
+    # 商品的总销售量
+    totalSales = models.IntegerField(default=0)
+    # 商品的月销售量
+    monthlySales = models.IntegerField(default=0)
+    # 商品的周销售量
+    weeklySales = models.IntegerField(default=0)
+    # 商品的日销售量
+    dailySales = models.IntegerField(default=0)
+    # 计算平均评分
+    def getAverageScore(self):
+        if self.scoreCnt == 0:
+            return 0
+        else:
+            return float(self.scoreSum) / self.scoreCnt
+    # 调试输出
+    def __str__(self):
+        return 'Shop.models.Goods(' + [
+            'id=' + toStringWithoutError(self.id),
+            'name=' + toStringWithoutError(self.name),
+            'description=' + toStringWithoutError(self.description),
+            'picture=' + toStringWithoutError(self.picture),
+            'shopId=' + toStringWithoutError(self.shopId),
+            'addTime=' + toStringWithoutError(self.addTime),
+            'scoreCnt=' + toStringWithoutError(self.scoreCnt),
+            'scoreSum=' + toStringWithoutError(self.scoreSum),
+            'totalSales=' + toStringWithoutError(self.totalSales),
+            'monthlySales=' + toStringWithoutError(self.monthlySales),
+            'weeklySales=' + toStringWithoutError(self.weeklySales),
+            'dailySales=' + toStringWithoutError(self.dailySales),
+        ].join(', ') + ')'
+
--- User/models.py
+++ User/models.py
@@ -1,3 +1,165 @@
 from django.db import models
 
-# Create your models here.
+# 把一坨东西转化为字符串，强行回避所有异常
+def toStringWithoutError(obj):
+    try:
+        return str(obj)
+    except Exception as e:
+        return str(e)
+
+# 把一坨东西转化为数字，强行回避所有异常
+def toIntegerWithoutError(obj):
+    try:
+        return int(obj)
+    except Exception as e:
+        return 0
+
+
+# 用户的信息
+class Users:
+    # 用户在数据库中的编号
+    id = models.IntegerField(primary_key=True)
+    # TODO 用户在微信中的基本信息
+    wxOpenId = models.TextField(max_length=256)
+    wxNickName = models.TextField(max_length=256, default='')
+    WX_SEX_CHOICE = (
+        (0, '未知'),
+        (1, '男'),
+        (2, '女'),
+    )
+    wxSex = models.IntegerField(choices=WX_SEX_CHOICE, default=0)
+    # 首次登陆时间
+    signUpTime = models.DateTimeField(auto_now_add=True)
+    # 上次登陆时间
+    lastLoginTime = models.DateTimeField(auto_now=True)
+    # 收到的站内消息数量
+    messageCount = models.IntegerField(default=0)
+    # 身份信息
+    IDENTITY_CHOICES = (
+        (0, '普通用户'),
+        (1, '店铺老板'),
+        (2, '管理员'),
+    )
+    identity = models.IntegerField(choices=IDENTITY_CHOICES, default=0)
+    # 如果是管理员，就记录管理员密码，否则是None
+    passwordOfAdministrator = models.TextField(null=True, default=None)
+    # 如果是店铺老板，就记录自己的店铺的编号，否则是None
+    isTheOwnerOfWhich = models.IntegerField(null=True, default=None)
+    # 收货地址和联系方式的数量
+    addressCount = models.IntegerField(default=0)
+    # 收货地址和联系方式的列表，以 "姓名|电话|地址#姓名|电话|地址#...#姓名|电话|地址"的形式存储
+    addressListStr = models.TextField(max_length=65536, default='[]')
+    # 获取地址列表，[[姓名, 电话, 地址], ...]
+    def getAddressList(self):
+        addressStrList = self.addressListStr.split('#')
+        addressList = []
+        for addressStr in addressStrList:
+            addressList.append(addressStr.split('|'))
+        return addressList
+    # 调试输出
+    def __str__(self):
+        return 'User.models.Users(' + [
+            'id=' + toStringWithoutError(self.id),
+            'wxOpenId=' + toStringWithoutError(self.wxNickName),
+            'wxNickName=' + toStringWithoutError(self.wxNickName),
+            'wxSex=' + toStringWithoutError(self.wxSex),
+            'signUpTime=' + toStringWithoutError(self.signUpTime),
+            'lastLoginTime=' + toStringWithoutError(self.lastLoginTime),
+            'messageCount=' + toStringWithoutError(self.messageCount),
+            'identity=' + toStringWithoutError(self.identity),
+            'passwordOfAdministrator=' + toStringWithoutError(self.passwordOfAdministrator),
+            'isTheOwnerOfWhich=' + toStringWithoutError(self.isTheOwnerOfWhich),
+            'addressCount=' + toStringWithoutError(self.addressCount),
+            'addressListStr=' + toStringWithoutError(self.addressListStr),
+        ].join(', ') + ')'
+
+
+# 每条用户收藏店铺记录的信息
+class CollectS:
+    # 这条记录在数据库中的编号
+    id = models.IntegerField(primary_key=True)
+    # 用户的编号
+    userId = models.IntegerField()
+    # 店铺的编号
+    shopId = models.IntegerField()
+    # 这条记录添加的时间
+    addTime = models.DateTimeField()
+    # 调试输出
+    def __str__(self):
+        return 'User.models.CollectS(' + [
+            'id=' + toStringWithoutError(self.id),
+            'userId=' + toStringWithoutError(self.userId),
+            'shopId=' + toStringWithoutError(self.shopId),
+            'addTime=' + toStringWithoutError(self.addTime),
+        ].join(', ') + ','
+
+
+# 每条用户收藏商品记录的信息
+class CollectG:
+    # 这条记录在数据库中的编号
+    id = models.IntegerField(primary_key=True)
+    # 用户的编号
+    userId = models.IntegerField()
+    # 商品的编号
+    goodsId = models.IntegerField()
+    # 这条记录添加的时间
+    addTime = models.DateTimeField()
+    # 调试输出
+    def __str__(self):
+        return 'User.models.CollectG(' + [
+            'id=' + toStringWithoutError(self.id),
+            'userId=' + toStringWithoutError(self.userId),
+            'goodsId=' + toStringWithoutError(self.goodsId),
+            'addTime=' + toStringWithoutError(self.addTime),
+        ].join(', ') + ')'
+
+
+# 每个用户订单的信息
+class Orders:
+    # 这条记录在数据库中的编号
+    id = models.IntegerField(primary_key=True)
+    # 用户的编号
+    userId = models.IntegerField()
+    # 订单使用用户的哪一个收货地址
+    userAddressId = models.IntegerField(default=0)
+    # 店铺的编号
+    shopId = models.IntegerField()
+    # 订单中的商品种类数量
+    goodsCount = models.IntegerField(default=0)
+    # 订单中商品的编号和数量的列表，用 "编号|数量#编号|数量#...#编号|数量" 的形式存储
+    goodsListStr = models.TextField(max_length=65536, default='[]')
+    # 下单时间
+    addTime = models.DateTimeField(auto_now_add=True)
+    # 订单的状态
+    STATE_CHOICE = (
+        (0, '下单后配送中'),
+        (1, '用户取消'),
+        (2, '商家取消'),
+        (3, '已完成待评论'),
+        (4, '已评论'),
+    )
+    state = models.IntegerField(choices=STATE_CHOICE, default=0)
+    # 如果已评论，就要存评论的分数
+    feedbackScore = models.IntegerField(null=True, default=None)
+    # 如果已评论，就要存评论的文字内容
+    feedbackContent = models.TextField(max_length=65536, default=None)
+    # 获取订单中的商品和数量列表，[[商品]]
+    def getGoodsList(self):
+        goodsStrList = self.goodsListStr.split('#')
+        goodsList = []
+        for goodsStr in goodsStrList:
+            goods = goodsStr.split('|')
+            goodsList.append(int(goods[0], int(goods[1])))
+        return goodsList
+    def __str__(self):
+        return 'User.models.Orders)' + [
+            'id=' + toStringWithoutError(self.id),
+            'userId=' + toStringWithoutError(self.userId),
+            'userAddressId=' + toStringWithoutError(self.userAddressId),
+            'shopId=' + toStringWithoutError(self.shopId),
+            'goodsListStr=' + toStringWithoutError(self.goodsListStr),
+            'addTime=' + toStringWithoutError(self.addTime),
+            'state=' + toStringWithoutError(self.state),
+            'feedbackScore=' + toStringWithoutError(self.feedbackScore),
+            'feedbackContent=' + toStringWithoutError(self.feedbackContent),
+        ].join(', ') + ')'
